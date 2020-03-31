9b2f8af0a364eca028731d1dfa8946aad131ade7
George Sued <33437116+GeorgeSued14@users.noreply.github.com>
2020-03-31 14:53:52 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const routes = require('./routes')
 const cors = require('cors')
 
+const port = env.process.PORT || 3333;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
